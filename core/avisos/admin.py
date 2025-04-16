0db8a19792697da5d37ee121a974f8f39071a9ea
Pedro <pguerrape@gmail.com>
2025-04-16 15:29:28 -0300

    alteração admin avisos

--- core/avisos/admin.py
+++ core/avisos/admin.py
@@ -1,3 +1,9 @@
+# avisos/admin.py
 from django.contrib import admin
+from .models import Aviso
 
-# Register your models here.
+@admin.register(Aviso)
+class AvisoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'categoria', 'data_publicacao', 'data_expiracao', 'autor')
+    search_fields = ('titulo', 'conteudo')
+    list_filter = ('categoria', 'data_publicacao')
