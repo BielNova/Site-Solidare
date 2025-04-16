c617b87ab949f2532acac3ae721bea3e363bf099
Pedro <pguerrape@gmail.com>
2025-04-16 15:49:34 -0300

    alteração apps de avisos

--- core/avisos/apps.py
+++ core/avisos/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class AvisosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'avisos'
+    name = 'core.avisos'  # Atualize para refletir o caminho completo
