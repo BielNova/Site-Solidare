a2680594f8431c10d5c6b2341ee56b623d594666
Pedro <pguerrape@gmail.com>
2025-04-16 15:43:00 -0300

    atualização do views de avisos

--- core/avisos/views.py
+++ core/avisos/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Aviso
 
-# Create your views here.
+class AvisoListView(ListView):
+    model = Aviso
+    template_name = 'avisos/aviso_list.html'
+    context_object_name = 'avisos'
+
+class AvisoDetailView(DetailView):
+    model = Aviso
+    template_name = 'avisos/aviso_detail.html'
+    context_object_name = 'aviso'
+
+class AvisoCreateView(CreateView):
+    model = Aviso
+    template_name = 'avisos/aviso_form.html'
+    fields = ['titulo', 'conteudo', 'categoria', 'data_expiracao', 'anexo']
