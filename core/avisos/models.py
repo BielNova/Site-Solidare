28a2020c8c83e20218542112b5f8ff6af9043749
Pedro <pguerrape@gmail.com>
2025-04-16 15:05:47 -0300

    protótipo models avisos

--- core/avisos/models.py
+++ core/avisos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Aviso(models.Model):
+    CATEGORIAS = [
+        ('geral', 'Geral'),
+        ('fundamental', 'Ensino Fundamental'),
+        ('medio', 'Ensino Médio'),
+        ('tecnico', 'Curso Técnico'),
+    ]
+
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    categoria = models.CharField(max_length=20, choices=CATEGORIAS, default='geral')
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+    data_expiracao = models.DateTimeField(null=True, blank=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    anexo = models.FileField(upload_to='avisos/anexos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
