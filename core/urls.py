7f5159173c1d742565678108d225b7b8d7c2778c
Pedro <pguerrape@gmail.com>
2025-04-16 15:13:37 -0300

    protótipo urls avisos

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # <-- IMPORTANTE
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('avisos/', include('avisos.urls')),  # <-- Adiciona essa linha
 ]
