0065308bb6016447e927fba76321dfb6a4e76968
Pedro <pguerrape@gmail.com>
2025-04-17 11:15:07 -0300

    implementação da tela de avisos sem css

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core.avisos.apps.AvisosConfig',
+    'core.avisos',
 ]
 
 MIDDLEWARE = [
