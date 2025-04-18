df556749ce6ef9e2080711558a69f6cdaef0fe39
Pedro <pguerrape@gmail.com>
2025-04-18 20:14:05 -0300

    registrando modelos de avisos no admin

--- core/avisos/admin.py
+++ core/avisos/admin.py
@@ -1,6 +1,11 @@
 # avisos/admin.py
 from django.contrib import admin
 from .models import Aviso
+from .models import Cliente, Transacao
+
+admin.site.register(Cliente)
+admin.site.register(Transacao)
+
 
 @admin.register(Aviso)
 class AvisoAdmin(admin.ModelAdmin):
