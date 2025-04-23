679b9420074b25b61fd5ef05a754fcda9fb2ef8e
Gabriel Assef Nova <gan2@cesar.school>
2025-04-23 11:59:39 -0300

    correção de erros

--- core/avisos/urls.py
+++ core/avisos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import AvisoListView, AvisoDetailView, AvisoCreateView
 
 urlpatterns = [
-    path('', AvisoListView.as_view(), name='aviso_list'),
-    path('novo/', AvisoCreateView.as_view(), name='aviso_create'),
-    path('<int:pk>/', AvisoDetailView.as_view(), name='aviso_detail'),
+    path('', AvisoListView.as_view(), name='aviso-list'),
+    path('<int:pk>/', AvisoDetailView.as_view(), name='aviso-detail'),
+    path('novo/', AvisoCreateView.as_view(), name='aviso-create'),
 ]
--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse  # Import necessário para criar uma resposta simples
+from django.http import HttpResponse
 
 def documentacao_view(request):
     return HttpResponse("<h1>Bem-vindo à página de Documentação</h1>")
@@ -12,7 +12,6 @@ urlpatterns = [
     path('', include('core.home.urls')),  # Inclui URLs do app home na raiz
     path('avisos/', include('core.avisos.urls')),
     path('user/', include('core.user.urls')),
-
-    path('user/', include('core.home.urls')),
+    # Removida a linha duplicada: path('user/', include('core.home.urls')),
     path('documentacao/', documentacao_view),  # Adiciona a rota para documentacao
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
