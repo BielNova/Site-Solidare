459762060de3a5f01292d82ccf6c146bafed4855
Gabriel Assef Nova <gan2@cesar.school>
2025-04-22 23:55:47 -0300

    Resolvendo erros na pagina home, login e adicionando uma mensagem de que o usuario ja existe

---
+++ core/user/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("Este nome de usuário já está em uso.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Este e-mail já está em uso.")
+        return email
--- core/user/views.py
+++ core/user/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-# minhaapp/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def user(request):
     return render(request, 'user/login.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redireciona para a página inicial
+        else:
+            messages.error(request, 'Credenciais inválidas.')
+    return render(request, 'user/login.html', {'form_type': 'login'})
+
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, 'Cadastro realizado com sucesso! Faça login para continuar.')
+            return redirect('login')
+        else:
+            # Não adiciona mensagens de erro aqui, pois o template já exibirá os erros do formulário
+            # Apenas indica que deve mostrar o formulário de registro
+            return render(request, 'user/login.html', {'form': form, 'show_register': True})
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'user/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Você saiu da sua conta.')
+    return redirect('login')
