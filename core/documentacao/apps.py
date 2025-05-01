4cd11d56d08a47f6040a809e2d12bdeef4738619
Gabriel Assef Nova <gan2@cesar.school>
2025-04-30 22:48:48 -0300

    Delete core/documentacao directory

--- core/documentacao/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DocumentacaoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.documentacao' # Corrigido para incluir 'core.'
-
