291ed06622cbc37be6c0f905da0a709f38c13d07
Gabriel Assef Nova <gan2@cesar.school>
2025-04-30 23:04:08 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('', include('core.home.urls')),  # Inclui URLs do app home na raiz
     path('avisos/', include('core.avisos.urls')),
     path('user/', include('core.user.urls')),
-    path('frequencia/', include('core.frequencia.urls', namespace='frequencia')),
-    path('documentacao/', include('core.documentacao.urls', namespace='documentacao')),
+    # path('frequencia/', include('core.frequencia.urls', namespace='frequencia')),
+    # path('documentacao/', include('core.documentacao.urls', namespace='documentacao')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
