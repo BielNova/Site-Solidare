8a4b603123a779cc7b29444cd6ab96168e8e3a5c
Gabriel Assef Nova <gan2@cesar.school>
2025-05-02 17:57:04 -0300

    Update urls.py

--- core/user/urls.py
+++ core/user/urls.py
@@ -5,15 +5,15 @@ from . import views
 
 urlpatterns = [
     # URL para a view de login
-    # Use name=\'login\' na tag {% url %} no template
-    path(\'login/\', views.login_view, name=\'login\'), 
+    # Use name='login' na tag {% url %} no template
+    path('login/', views.login_view, name='login'), 
     
     # --- NOVA URL PARA A VIEW DE REGISTRO --- 
-    # Use name=\'register\' na tag {% url %} no template
-    path(\'register/\', views.register_view, name=\'register\'),
+    # Use name='register' na tag {% url %} no template
+    path('register/', views.register_view, name='register'),
     
     # URL para a view de logout (exemplo)
-    path(\'logout/\', views.logout_view, name=\'logout\'),
+    path('logout/', views.logout_view, name='logout'),
     
     # Adicione outras URLs do app user aqui, se necessário
 ]
