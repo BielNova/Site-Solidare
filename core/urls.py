5c9fa695825dac43d9d106119896d5bae1b44f27
BielNova <gan2@cesar.school>
2025-05-06 11:01:38 -0300

    Adição do app profesor no urls.py

--- core/urls.py
+++ core/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('user/', include('core.user.urls')),
     # path('frequencia/', include('core.frequencia.urls', namespace='frequencia')),
     # path('documentacao/', include('core.documentacao.urls', namespace='documentacao')),
+    path("professor/", include("core.professor.urls")), # Adiciona URLs do app professor
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
