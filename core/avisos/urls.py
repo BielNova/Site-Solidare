647457f735ac2c9e807a7d1bd887b0aee93f71b6
BielNova <gan2@cesar.school>
2025-05-06 19:08:34 -0300

    Correção do urls avisos para direcionar para dashboard

--- core/avisos/urls.py
+++ core/avisos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import AvisoListView, AvisoDetailView, AvisoCreateView
 
+app_name = 'avisos'
+
+
 urlpatterns = [
     path('', AvisoListView.as_view(), name='aviso_list'),
     path('novo/', AvisoCreateView.as_view(), name='aviso_create'),
