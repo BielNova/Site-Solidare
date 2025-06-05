6ab4a0c8b6a25b5827e2f7350e13b0dc9bd97458
Pedro Marinho <pmpc@cesar.school>
2025-06-05 18:00:42 -0300

    Update models.py

--- core/avisos/models.py
+++ core/avisos/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-class Cliente(models.Model):
-    nome = models.CharField(max_length=100)
-    # outros campos...
+
 
 class Aviso(models.Model):
     CATEGORIAS = [
