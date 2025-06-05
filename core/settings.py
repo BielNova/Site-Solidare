2f6758f9c9841056f372ad662561755c4a7bb88a
Pedro Marinho <pmpc@cesar.school>
2025-06-05 00:17:25 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'core.avisos',
+    'core.avisos',
     'core.user',
     'core.home',
     'core.professor',
